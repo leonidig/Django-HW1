6a49b15a16a8ac6504665171dd4662840e63b67e
Leonidig <leonid200955@gmail.com>
2025-02-04 18:37:48 +0200

    models done

--- homework/blog/models.py
+++ homework/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    added_at = models.DateTimeField(auto_now_add=True)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="products")
+
+
+class Order(models.Model):
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="orders")
+    quantity = models.PositiveIntegerField()
+    ordered_at = models.DateTimeField(auto_now_add=True)
+
+    def __repr__(self):
+        return f"Order({self.buyer}; {self.product}; {self.quantity}, {self.ordered_at})"
